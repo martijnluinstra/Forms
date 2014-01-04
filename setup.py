e0ce609298bda02305b651487fb55685cbb458b2
Martijn Luinstra <martijnluinstra@gmail.com>
2014-01-04 20:54:34 +0100

    Add more default data

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from forms import db
-from forms.models import User, Committee
+from forms.models import User, Committee, Activity, Form, Field
+from datetime import datetime
 
 # Create the database
 db.create_all()
@@ -13,4 +14,21 @@ db.session.add(c_admin)
 db.session.commit()
 
 u_martijn.committees.append(c_admin)
+db.session.commit()
+
+activity1 = Activity('Activity1', 'test', c_admin.id, 'Groningen', datetime.utcnow())
+
+db.session.add(activity1)
+db.session.commit()
+
+form1 = Form(activity1.id)
+
+db.session.add(form1)
+db.session.commit()
+
+field1 = Field(form1.id, 'Field1', 0, 0)
+field2 = Field(form1.id, 'Field2', 1, 0)
+
+db.session.add(field1)
+db.session.add(field2)
 db.session.commit()
