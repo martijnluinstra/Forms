7ab84fa8e59d075cda854634189e1ad663b33677
Martijn Luinstra <martijnluinstra@gmail.com>
2014-01-04 15:42:52 +0100

    Correct some errors and add constructors

--- forms/models.py
+++ forms/models.py
@@ -1,4 +1,5 @@
 from forms import db
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 users_committees_table = db.Table('users_committees',
@@ -37,43 +38,84 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+
 class Committee(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
-    rights = db.Column(db.Integer)
+    admin = db.Column(db.Boolean)
+
+    def __init__(self, name, admin=False):
+        self.name = name
+        self.admin = admin
 
 
 class Activity(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
-    description = db.Column(db.String())
-    start_time = db.Column(db.DateTime())
-    end_time = db.Column(db.DateTime(), nullable=True)
+    description = db.Column(db.String)
+    committee_id = db.Column(db.Integer, db.ForeignKey('committee.id'))
     location = db.Column(db.String(80))
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime, nullable=True)
+
+    def __init__(self, name, description, committee_id, location, start_time, end_time=None):
+        self.name = name
+        self.description = description
+        self.committee_id = committee_id
+        self.location = location
+        self.start_time = start_time
+        self.end_time = end_time
   
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'))
-    deadline = db.Column(db.DateTime())
+    deadline = db.Column(db.DateTime)
     fields = db.relationship('Field', backref='form',
                               lazy='dynamic')
     entries = db.relationship('Entry', backref='form',
                                lazy='dynamic')
 
+    def __init__(self, activity_id, deadline):
+        self.name = name
+        self.activity_id = activity_id
+        self.deadline = deadline
+
 
 class Field(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    form_id = db.Column(db.Integer, db.ForeignKey('form.id'))
     label = db.Column(db.String(80))
+    display_index = db.Column(db.Integer)
     data_type = db.Column(db.Integer)
     required = db.Column(db.Boolean)
+
+    def __init__(self, form_id, label, display_index, data_type, required=True):
+        self.label = label
+        self.form_id = form_id
+        self.display_index = display_index
+        self.data_type = data_type
+        self.required = required
 
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    form_id = db.Column(db.Integer, db.ForeignKey('form.id'))
+    timestamp = db.Column(db.DateTime)
     values = db.relationship('EntryValue')
+
+    def __init__(self, form_id):
+        self.form_id = form_id
+        self.timestamp = datetime.utcnow()
+
 
 class EntryValue(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    field = db.Column(db.Integer, db.ForeignKey('Field.id'))
+    entry_id = db.Column(db.Integer, db.ForeignKey('entry.id'))
+    field_id = db.Column(db.Integer, db.ForeignKey('field.id'))
     value = db.Column(db.String)
+
+    def __init__(self, field, value, entry):
+        self.entry = entry_id
+        self.field = field_id
+        self.value = value
