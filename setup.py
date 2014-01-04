a1c9b2d28b1671cc2623c3cff7b9638624aca14c
Martijn Luinstra <martijnluinstra@gmail.com>
2014-01-04 15:43:14 +0100

    Create some default data

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from forms import db
+from forms.models import User, Committee
 
 # Create the database
 db.create_all()
 
 # Create some users
+u_martijn = User('Martijn', 'test', 'martijn@svcover.nl')
+c_admin = Committee('admin', True)
+
+db.session.add(u_martijn)
+db.session.add(c_admin)
+db.session.commit()
+
+u_martijn.committees.append(c_admin)
+db.session.commit()
